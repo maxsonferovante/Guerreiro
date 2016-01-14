64017942d1001481cee1a1a496ab10a8cc4cfa1e
maxsonferovante <maxsonferovante@gmail.com>
2016-01-14 12:43:17 -0300

    Criado os metodos Set e Get para os atributos da classe Guerreiro. Criado variaveis const int para controle do valor maximo dos atributos.
    ERRO encontrado, ainda falta resolver: Guerreiro::Guerreiro(std::string,
    int, int, int, int, int)' does not match any in class 'Guerreiro'

--- Guerreiro.cpp
+++ Guerreiro.cpp
@@ -1,2 +1,54 @@
+#include"Guerreiro.h"
 
+Guerreiro::Guerreiro ( string N, int S, int A, int I, int L, int M);{
+	void setNomedoGuerreiro (N);
+	void setStrenghtdoGuerreiro (S);
+	void setAgilitydoGuerreiro (A);
+	void setIntelligencedoGuerreiro (I);
+	void setLifedoGuerreiro (L);
+	void setManadoGuerreiro (M);
+}
+void Guerreiro::setNomedoGuerreiro ( string nome){
+	nomedoGuerreiro = nome;
+}
+void Guerreiro::setStrenghtdoGuerreiro ( int S){
+	strenght = S;
+}
+void Guerreiro::setAgilitydoGuerreiro ( int A){
+	agility = A;
+}
+void Guerreiro::setIntelligencedoGuerreiro ( int I){
+	intelligence = I;
+}
+void Guerreiro::setLifedoGuerreiro ( int L){
+	life = L;
+}
+void Guerreiro::setManadoGuerreiro ( int M){
+	mana = M;
+}
+
+string Guerreiro::getNomedoGuerreiro (){
+	return nomedoGuerreiro;	
+}
+int Guerreiro::getStrenghtdoGuerreiro (){
+	return strenght;
+}
+int Guerreiro::getAgilitydoGuerreiro (){
+	return agility;
+}
+int Guerreiro::getIntelligencedoGuerreiro (){
+	return intelligence;
+}
+int Guerreiro::getLifedoGuerreiro (){
+	return life;
+}
+int Guerreiro::getManadoGuerreiro (){
+	return mana;
+}
+void Guerreiro::treinarGuerreiro (){
+	cout<<"O Guerreiro "<<getNomedoGuerreiro()<<" esta treinando suas habilidades...";
+	Sleep(2000);
+}
+bool Guerreiro::regenarGuerreiro (){
+	
 }
--- Guerreiro.h
+++ Guerreiro.h
@@ -1,19 +1,40 @@
 #include<iostream>
 #include<string>
+#include<windows.h>
 
-sdt::cout;
-sdt::string;
-sdt::getline;
+using namespace std;
 
 class Guerreiro
 {
 	private:
 		int strenght;
 		int agility;
-
-	public:
-		Guerreiro (string);
-		void setNomedoGuerreiro( string );
-		string getNomedoGuerreiro( string );
+		int intelligence;
+		int life;
+		int mana;
+		string nomedoGuerreiro;
 		
-}
+		const int strenghtMaxima = 100;
+		const int agilityMaxima = 100;
+		const int intelligenceMaxima = 100;
+		const int lifeMaxima = 100;
+		const int manaMaxima = 100;
+	public:
+		Guerreiro ( string, int, int, int, int, int, int);
+		void setNomedoGuerreiro ( string );
+		void setStrenghtdoGuerreiro ( int );
+		void setAgilitydoGuerreiro ( int );
+		void setIntelligencedoGuerreiro ( int );
+		void setLifedoGuerreiro ( int );
+		void setManadoGuerreiro ( int );
+		
+		string getNomedoGuerreiro ();
+		int getStrenghtdoGuerreiro ();
+		int getAgilitydoGuerreiro ();
+		int getIntelligencedoGuerreiro ();
+		int getLifedoGuerreiro ();
+		int getManadoGuerreiro ();
+		
+		void treinarGuerreiro ();
+		bool regenarGuerreiro ();
+};
