f9d1ae2ecf8d9ab5e06e46785d775d3454b40ffd
maxsonferovante <maxsonferovante@gmail.com>
2016-01-14 11:32:39 -0300

    Inicio do Projeto. Declarado os primeiros atributos e metodos set e get.

---
+++ Guerreiro.h
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+
+sdt::cout;
+sdt::string;
+sdt::getline;
+
+class Guerreiro
+{
+	private:
+		int strenght;
+		int agility;
+
+	public:
+		Guerreiro (string);
+		void setNomedoGuerreiro( string );
+		string getNomedoGuerreiro( string );
+		
+}
