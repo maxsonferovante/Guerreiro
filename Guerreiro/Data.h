5d8353b208b63ba5f58c925e466b5d3de1f6c54d
Maxson Almeida Ferovante <maxsonferovante@gmail.com>
2016-02-15 18:33:31 -0300

    inclusão do método  static 'atualizacaodoEquipamento' e da classe Data no diagrama UML.

---
+++ Guerreiro/Data.h
@@ -0,0 +1,16 @@
+#ifndef DATA_H
+#define DATA_H
+class Data
+{
+    
+public:
+    Data(int = 1, int = 1 , int = 1900);
+    ~Data();
+private:
+        int month;
+        int day;
+        int year;
+        
+        int checkday(int ) const;
+};
+#endif
