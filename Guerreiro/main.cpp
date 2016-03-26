5e1d8cbec079e0b40266cfd33eeed66604211ba9
maxson almeida ferovante <maxsonferovante@gmail.com>
2016-03-26 11:44:40 -0300

    Criado a escolha do personagem no main.

--- Guerreiro/main.cpp
+++ Guerreiro/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <typeinfo>
+#include <stdlib.h>
+#include <time.h>
 #include "Personagem.h"
 
 #include "Espada.h"
@@ -12,13 +14,40 @@ using std::cin;
 using std::vector;
 
 int main(){
-    vector < Personagem *> personagens;
-    personagens.push_back(new Espartano(1,"Caloteira",29,02,1996,new Espada()));
-    personagens.push_back(new Espartano(2,"Migo",21,04,1997,new Espada()));
-    personagens.push_back(new Espartano(1,"Mexson",14,10,1994,new Espada()));
+    srand (time(NULL));
     
-    personagens.push_back(new Gandalf("Tavares",12,12,1212,50));    
-    personagens.push_back(new Gandalf("Antonio",21,01,1333,25));    
+    vector < Personagem *> personagens;
+    int op, sorteado, cenario;
+    sorteado = rand()%2;
+    cenario = rand()%3;
+    
+    cout<<"\nHa um sorteio para decidir a ordem de escolha do personagens.\nO player escolhido poderá escolher seu personagem.\n";
+    
+    do{
+        cout<<"\n 0 - Para O Mago Gandalf\t 1 - Para o Guerreiro Kratos.";
+        cout<<"\nEscolha seu personagem, player "<< sorteado+1 <<":";
+        cin>> op;
+        switch (op)
+        {
+            case 0:
+            {
+                personagens.push_back(new Gandalf("Gandalf",21,01,1333,25));
+                personagens.push_back(new Espartano(1,"Kratos",29,02,300,new Espada()));            
+                op = 5;
+            }break;
+            
+            case 1:
+            {
+                personagens.push_back(new Espartano(1,"Kratos",29,02,300,new Espada()));            
+                personagens.push_back(new Gandalf("Gandalf",21,01,1333,25));
+                op = 5;
+            }break;
+            default:
+                cout<<"Opcao invalida.";
+                system("cls");
+        }
+    }while(op!=5);
+    
     
     for (int i = 0;i<personagens.size(); i++)
     {
@@ -26,14 +55,16 @@ int main(){
         if (gandalfPtr !=0)
         {
             gandalfPtr->aumentarXp();
+            gandalfPtr->aumentarXp();
             cout<<"\nNovo Xp do Personagem "<<gandalfPtr->getNome()<<" eh "<<gandalfPtr->getXp();
         }
         cout<<"\nO Xp do Personagem "<<personagens[i]->getNome()<<" eh "<<personagens[i]->getXp();
     }
     
+    
     for (int i = 0; i< personagens.size(); i++)
     {
-        cout << "deleting object of "<< typeid( *personagens[ i ] ).name() << std::endl;
+        cout << "\ndeleting object of  "<< typeid( *personagens[ i ] ).name() << std::endl;
         delete personagens[ i ];
     }
     
